659bd3df0ea606965876aaa20283164cdbaa1056
Mrinmoy <mrinmoydeb485@gmail.com>
2024-03-22 00:11:28 +0530

    Added minor functions

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let userData = document.querySelector('.inputbox input');
 let addBtn = document.querySelector('.inputbox button');
 let list = [];
+let listItem = document.querySelector('.list');
+
 
 addBtn.addEventListener('click', ()=>{
     let data = userData.value;
     list.push(data)
-   
+    setVal()
 })
-let txt = ''
-for(let i = 0; i<list.length; i++){
-  txt += list[i]
+function setVal(){
+    list.forEach((val)=>{
+        let li = document.createElement('li')
+        li.innerHTML =`<li><span>${val}</span> <span>X</span></li>`
+        listItem.appendChild(li)
+    })
 }
-console.log(list)
 
