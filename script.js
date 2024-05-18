473436ee1145eaf9f3ca016e2de8f93ceb2a5c4a
Mrinmoy deb <mrinmoydeb485@gmail.com>
2024-05-18 21:43:39 +0530

    minor changes

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 // let list = [];
 // let listItem = document.querySelector('.list');
 
-
 // addBtn.addEventListener('click', ()=>{
 //     let data = userData.value;
 //     list.push(data)
@@ -22,21 +21,52 @@ const add_button = document.querySelector(".inputbox button");
 const task_list = document.querySelector(".list");
 const err = document.querySelector(".err");
 
-add_button.onclick = ()=>{
+
+add_button.onclick = () => {
     let taskText = entry.value.trim();
-    if(taskText !== ""){
-        let li = document.createElement('li');
-        li.textContent = taskText;
+    var doneButton = document.createElement("input");
+    var showval = document.createElement("p");
+    if (taskText !== "") {
+        let li = document.createElement("li");
         task_list.appendChild(li);
-        err.style.display = "none"
-        entry.style.border = 'none';
+        let span_two = document.createElement("span");
+        let span_one = document.createElement("span");
+
+        span_one.onclick = completeTask;
+        showval.textContent = taskText;
+      
+        doneButton.type = "checkbox";
+        span_one.appendChild(doneButton);
+        span_one.appendChild(showval);
+        span_two.innerText = "Delete";
+        span_two.onclick = onDelete;
+        li.appendChild(span_one);
+        li.appendChild(span_two);
+        err.style.display = "none";
+        entry.style.border = "none";
         entry.value = "";
-    }else{
-        err.style.display = "block"
-        entry.style.border = '1px solid red';
-        err.innerText = "Value can't be empty !"
+    } else {
+        err.style.display = "block";
+        entry.style.border = "1px solid red";
+        err.innerText = "Value can't be empty !";
     }
-   
+
+    function completeTask(e) {
+        // gug need to be fix on done button
+        let t = e.target;
+        t.onclick = () => {
+            if (doneButton.checked) {
+                showval.classList.add("done");
+            } else {
+                showval.classList.remove("done");
+            };
+        }
+
+    }
+};
+
+function onDelete(e) {
+    let task = e.target
+    task.parentElement.remove();
 }
-
-
+ 
