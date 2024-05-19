1d845c6e6012bfbf7255b80d5b86beb38192bab9
Mrinmoy Deb <mrinmoydeb485@gmail.com>
2024-05-20 00:59:49 +0530

    Add tasks implemented

--- script.js
+++ script.js
@@ -1,51 +1,41 @@
-// let userData = document.querySelector('.inputbox input');
-// let addBtn = document.querySelector('.inputbox button');
-// let list = [];
-// let listItem = document.querySelector('.list');
-
-// addBtn.addEventListener('click', ()=>{
-//     let data = userData.value;
-//     list.push(data)
-//     setVal()
-// })
-// function setVal(){
-//     list.forEach((val)=>{
-//         let li = document.createElement('li')
-//         li.innerHTML =`<li><span>${val}</span> <span>X</span></li>`
-//         listItem.appendChild(li)
-//     })
-// }
-
 const entry = document.querySelector(".inputbox input");
 const add_button = document.querySelector(".inputbox button");
 const task_list = document.querySelector(".list");
+const countValue = document.querySelector(".pending-task");
 const err = document.querySelector(".err");
-
+let taskCount = 0;
 
 add_button.onclick = () => {
-    let taskText = entry.value.trim();
-    if (taskText !== "") {
-        let li = document.createElement("li");
+  let taskText = entry.value.trim();
+  if (taskText !== "") {
+    const task = `<li class="li-list">
+       <input type="checkbox" class="check-box">
+       <span class="task-name">${taskText}</span>
+       <button class="editBtn">Edit</button>
+       <button class="dltBtn">Delete</button>
+       </li>`;
+    task_list.insertAdjacentHTML("beforeend", task);
+    let dltButtos = document.querySelectorAll(".dltBtn");
+    dltButtos.forEach((dButtons) => {
+      dButtons.onclick = () => {
+        dButtons.parentNode.remove();
+        taskCount = -1;
+        displayCount(taskCount);
+      };
+    });
 
-        entry.value = "";
-
-
-
-        // validation 
-        err.style.display = "none";
-        entry.style.border = "none";
-
-    } else {
-
-
-        
-         // validation 
-        err.style.display = "block";
-        entry.style.border = "1px solid red";
-        err.innerText = "Value can't be empty !";
-    }
-
-
+    entry.value = "";
+    // validation
+    err.style.display = "none";
+    entry.style.border = "none";
+  } else {
+    // validation
+    err.style.display = "block";
+    entry.style.border = "1px solid red";
+    err.innerText = "Value can't be empty !";
+  }
 };
 
-
+function displayCount(taskCount) {
+  countValue.innerText = taskCount;
+}
